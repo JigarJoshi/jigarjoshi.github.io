1c9933bab21bcb9b273b7fc1b56e81a1b163b330
Jigar Joshi <jmj@uber.com>
2020-07-07 23:38:06 -0700

    rename

---
+++ maps/covid19/common/map.js
@@ -0,0 +1,61 @@
+function renderMap() {
+  var map = new google.maps.Map(document.getElementById('map'), {
+    zoom: 13,
+    center: new google.maps.LatLng(centerOfMap.lat, centerOfMap.lng),
+    mapTypeId: google.maps.MapTypeId.ROADMAP
+  });
+
+  var infowindow = new google.maps.InfoWindow();
+
+  var marker, i;
+
+  for (i = 0; i < locations.length; i++) {
+    marker = new google.maps.Marker({
+      position: new google.maps.LatLng(parseFloat(locations[i][1]), parseFloat(locations[i][2])),
+      title: locations[i][0] + '_' + locations[i][3],
+      map: map
+    });
+
+    google.maps.event.addListener(marker, 'click', (function(marker, i) {
+      return function() {
+        infowindow.setContent("Name: " + locations[i][0] + '<br/> Addr: ' + locations[i][3]);
+        infowindow.open(map, marker);
+      }
+    })(marker, i));
+  }
+}
+var map;
+var marker;
+var latPosition;
+var longPosition;
+
+function initialize() {
+  var mapOptions = {
+    zoom: 20,
+    gestureHandling: 'cooperative',
+    mapTypeId: google.maps.MapTypeId.ROADMAP,
+  };
+  initializeMarker();
+}
+
+function initializeMarker() {
+  if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(function(position) {
+
+      var pos = new google.maps.LatLng(position.coords.latitude, position.coords.longitude);
+
+      latPosition = position.coords.latitude;
+      longPosition = position.coords.longitude;
+
+      marker = new google.maps.Marker({
+        position: pos,
+        map: map,
+      });
+
+      map.setCenter(pos);
+
+    });
+  }
+}
+
+initialize();
